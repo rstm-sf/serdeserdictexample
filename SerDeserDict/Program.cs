99a5777c12fae1bdbfaffd3c2eedfd2dba28bf3d
Rustam Sayfutdinov <rstm.sf@gmail.com>
2019-06-05 01:06:16 +0300

    Первый коммит

---
+++ SerDeserDict/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerDeserDict
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dict_1 = new SerializableDictionary<int, List<Value>>
+            {
+                { 1, new List<Value>() { new Value { Id = 1, Text = "text 1" } } }
+            };
+
+            var type = typeof(SerializableDictionary<int, List<Value>>);
+            var ser = new XmlSerializer(type);
+            string str;
+            SerializableDictionary<int, List<Value>> dict_2;
+
+            using (var stream = new MemoryStream())
+            {
+                ser.Serialize(stream, dict_1);
+                stream.Position = 0;
+                str = System.Text.Encoding.Default.GetString(stream.ToArray());
+                dict_2 = (SerializableDictionary<int, List<Value>>)ser.Deserialize(stream);
+            }
+
+            Console.WriteLine(str + '\n');
+            Console.WriteLine(dict_2[1][0].Text);
+        }
+    }
+}
---
+++ SerDeserDict/Value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerDeserDict
+{
+    [Serializable]
+    public class Value
+    {
+        public int Id { set; get; }
+
+        public string Text { set; get; }
+    }
+}
